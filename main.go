a70959046dd2b12ad28ea8e76a12528ffd796054
handaru <handarusakti@gmail.com>
2015-11-17 15:05:29 +0700

    Return of Func

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
-package main
+package pustaka
 
-func main() {
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
 
-}
-
-func hi(name string) string {
-	return name
+	return string(r)
 }
